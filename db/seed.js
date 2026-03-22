f3b3fb914cd1cd38474678d5ad2d9b5d36a6d956
jaytrinidadpr <justinquelix@icloud.com>
2026-03-21 20:18:56 -0500

    seed and app updated

--- db/seed.js
+++ db/seed.js
@@ -8,5 +8,34 @@ console.log("🌱 Database seeded.");
 async function seed() {
   const folder = ["documents", "pictures", "music"];
 
-  for (const)
+  for (const folderName of folder) {
+    const folderResults = await db.query(
+      `
+      INSERT INTO folders (name) 
+      VALUES ($1) 
+      RETURNING *;
+      `,
+      [folderName]
+    );
+
+    const folder = folderResults.rows[0];
+
+    const files = [
+      ["file1.text", 100, folder.id],
+      ["file2.text", 200, folder.id],
+      ["file3.text", 300, folder.id],
+      ["file4.text", 400, folder.id],
+      ["file5.text", 500, folder.id],
+    ];
+
+    for (const file of files) {
+      await db.query(
+        `
+        INSERT INTO files (name, size, folder_id) 
+        VALUES ($1, $2, $3);
+        `,
+        file
+      );
+    }
+  }
 }
